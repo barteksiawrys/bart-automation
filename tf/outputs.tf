9825918cd38e9717b0f8c61a2c98109ea9791859
Bartlomiej Siawrys <bartlomiej.siawrys@nask.pl>
2023-12-27 16:02:47 +0100

    vm part is done

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,7 @@
 output "az_resource_group" {
   value = azurerm_resource_group.rg.name
 }
+
+output "az_vm_private_ip" {
+  value = azurerm_linux_virtual_machine.vm.private_ip_address
+}
--- tf/vm.tf
+++ tf/vm.tf
@@ -1,38 +1,38 @@
-resource "azurerm_network_interface" "nic-tf" {
-  name                = "nic-${azurerm_linux_virtual_machine.vmname}-"
-  location            = azurerm_resource_group.rg-tf.location
-  resource_group_name = azurerm_resource_group.rg-tf.name
+resource "random_uuid" "nic" {
+}
+
+resource "random_id" "nic" {
+  byte_length = 3
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vm-${var.vm_name}-${var.rg_name}-${var.name_suffix}-${random_id.nic.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet-tf.id
+    subnet_id                     = azurerm_subnet.subnet-db.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip-tf.id
-  }
-
-  tags = {
-    environment = "r&d"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "vm-tf"
-  resource_group_name   = azurerm_resource_group.rg-tf.name
-  location              = azurerm_resource_group.rg-tf.location
+  name                  = "vm-${var.vm_name}-${var.rg_name}-${var.name_suffix}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
   size                  = "Standard_B1ls"
-  admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.nic-tf.id]
-
-  custom_data = filebase64("customdata.tpl")
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    username   = "azureuser"
+    public_key = file("bastion_id_rsa.pub")
   }
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_ZRS"
   }
 
   source_image_reference {
@@ -40,18 +40,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
-  }
-
-  provisioner "local-exec" {
-    command = templatefile("${var.host_os}-ssh-script.tpl", {
-      hostname     = self.public_ip_address,
-      user         = "adminuser",
-      identityfile = "~/.ssh/id_rsa"
-    })
-    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
-  }
-
-  tags = {
-    environment = "r&d"
   }
 }
